9e149dee9b41c3228ca0b04a4229fb36137e5b58
Jacob Sambajon <jacobsambajon@gmail.com>
2021-01-06 19:15:36 -0800

    test

--- modules/module11/Activities/A1-Evr_Python_to_JavaScript/Unsolved/1-HelloVariableWorld/hello-variable-world.js
+++ modules/module11/Activities/A1-Evr_Python_to_JavaScript/Unsolved/1-HelloVariableWorld/hello-variable-world.js
@@ -1,24 +1,35 @@
 // Create a variable called "name" that holds a string
+var name = "Homer Simpson";
 
 // Create a variable called "country" that holds a string
+var country = "United States";
 
 // Create a variable called "age" that holds an integer
+var age = 25;
 
 // Create a variable called "hourlyWage" that holds an integer
-
-// Calculate the "dailyWage" for the user
+var hourly_wage = 15;
 
 // Create a variable that holds a number as a string
+var weekly_hours = "40";
+
+// Calculate the "dailyWage" for the user
+var daily_wage = hourly_wage * 8;
 
 // Create a variable called 'weeklyWage' that converts a string into an integer
+var weekly_wage = hourly_wage * parseInt(weekly_hours);
 
 // Create a variable called "satisfied" that holds a boolean
+var satisfied = True;
 
 // Print out "Hello <name>!"
+console.log("Hello name");
 
 // Print out what country the user entered
+console.log('You live in ${country}.');
 
 // Print out the user's age
+console.log("You are $(age) years old.");
 
 // Print out the daily wage that was calculated
 
